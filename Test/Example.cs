1077492045aa1b878f1fd839f10fa908eeca2ea0
Daniel Baur <daniel.baur@uni-ulm.de>
2015-02-11 10:39:38 +0100

    Automatically refresh tcp client. Report multiple metrics at once. Clarified example.

--- Test/Example.cs
+++ Test/Example.cs
@@ -1,4 +1,19 @@
-﻿using System;
+﻿// Copyright (c) 2015 University of Ulm
+// 
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.  Licensed under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,11 +27,28 @@ namespace Test
         static void Main(string[] args)
         {
             var telnetClient = new TelnetClient(9001, "localhost");
-            for (var i = 0; i < 100; i++)
+            var metricList = new HashSet<Metric>();            
+            // sends a single metric to the monitoring agent (one tcp connection per metric).
+            for (var i = 0; i < 20; i++)
             {
-                System.Threading.Thread.Sleep(2000);
-                telnetClient.Report(Metric.Builder().ApplicationName("testApplication").MetricName("testMetric").TimeStamp(GetCurrentUnixTimestamp()).Value("testValue").Build());
+                System.Threading.Thread.Sleep(1000);
+                Console.WriteLine("Reporting Metric to monitoring agent.");
+                var metric =
+                    Metric.Builder()
+                        .ApplicationName("testApplication")
+                        .MetricName("testMetric")
+                        .TimeStamp(GetCurrentUnixTimestamp())
+                        .Value("testValue")
+                        .Build();
+                metricList.Add(metric);
+                telnetClient.Report(metric);
             }
+            // sends multiple metrics to the monitoring agent (one tcp connection for all metrics)
+            Console.WriteLine("Sending "+metricList.Count+" metrics to monitoring agent.");
+            telnetClient.Report(metricList);
+
+            Console.WriteLine("Finished sending metrics. Closing application in 10 seconds.");
+            System.Threading.Thread.Sleep(10 * 1000);
         }
 
         protected static long GetCurrentUnixTimestamp()
